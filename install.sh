b673a55f57a0d9f348c473b411bea8c62a5c3e94
skullface <skullface@users.noreply.github.com>
2026-08-18 18:24:19 -0400

    bootstrap: use nvm + set node lts version

--- install.sh
+++ install.sh
@@ -35,22 +35,16 @@ echo -e "\\n\\n📦 Installing apps and CLI packages…"
 echo "=================================================="
 brew bundle
 
-echo -e "\\n\\n=================================================="
 echo "🚮 Cleaning up any old brews or casks…"
 echo "=================================================="
 brew cleanup
 
-# NVM
-echo -e "\\n\\n💚 Installing NVM and setting Node version…"
+# Node via fnm
+echo -e "\\n\\n💚 Installing Node with fnm…"
 echo "=================================================="
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-nvm install 20.11.1
-nvm alias default 20.11.1
+eval "$(fnm env)"
+fnm install --lts
+fnm default lts-latest
 
 # Customize Terminal: download and install custom font
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CommitMono.zip
